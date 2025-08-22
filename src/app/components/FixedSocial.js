5508703209a4d5d8d49f3b15f443b62f1d3d1bfe
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2025-08-22 19:56:40 +0530

    Chaneg profile photo 2

--- src/app/components/FixedSocial.js
+++ src/app/components/FixedSocial.js
@@ -5,11 +5,11 @@ import React from "react";
 
 const FixedSocial = () => {
   return (
-    <div className="fixed left-0 right-0  bottom-5 flex items-center justify-center ">
+    <div className="fixed left-0 right-0 z-10  bottom-5 flex items-center justify-center ">
       <div className="border border-gray-900 p-2 px-4 flex items-center justify-center gap-5 rounded-full bg-black">
         {socialLinks?.map((item, index) => {
           return (
-            <Link href={item?.href} target="_blank" >
+            <Link key={index} href={item?.href} target="_blank" >
               {item?.icon}
             </Link>
           );
